ed7ed071aab161da7d4568e75401293af7f0c678
PRANAV <pranav@PRANAVs-MacBook-Pro.local>
2023-11-03 12:13:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/contactus', (req, res) => {
+    res.sendFile(__dirname + '/contactus.html');
+})
+
+app.get('/tandc', (req, res) => {
+    res.sendFile(__dirname + '/tandc.html');
+})
+
+app.get('/privacy', (req, res) => {
+    res.sendFile(__dirname + '/privacy.html');
+})
+
+
+app.get('/refundandcancellation', (req, res) => {
+    res.sendFile(__dirname + '/refundandcancellation.html');
+})
+
+app.get('/shipanddelivery', (req, res) => {
+    res.sendFile(__dirname + '/shipanddelivery.html');
+})
+
+app.get('/checkout', (req, res) => {
+    res.sendFile(__dirname + '/checkout.html');
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
